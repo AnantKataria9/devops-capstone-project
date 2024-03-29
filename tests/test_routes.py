4b9654cadcd654c12bb8efce18a83e48a1cff00d
Anant Kataria <hr.kataria-maths@msubaroda.ac.in>
2024-03-29 07:43:33 -0400

    add test for list all accounts

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -177,3 +177,11 @@ class TestAccountService(TestCase):
         account = self._create_accounts(1)[0]
         response = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_list_accounts(self):
+        """It should list all accounts"""
+        accounts = self._create_accounts(10)
+        response = self.client.get(BASE_URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 10)
